ff69df7fc28f48c3e556dc129c8236bf50cd2203
Alex Chemus <aachemus@gmail.com>
2022-07-07 22:57:26 +0500

    update 404.html

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createStore } from 'vuex'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import initStore from './store'
 import routes from './routes'
@@ -9,7 +9,7 @@ import './style.scss'
 const store = createStore(initStore)
 const router = createRouter({
   routes,
-  history: createWebHistory()
+  history: createWebHashHistory()
 })
 
 createApp(App)
