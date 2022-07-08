2995c0a7007ef8421867817405e52e0302ab0ac7
Alex Chemus <aachemus@gmail.com>
2022-07-08 16:50:20 +0500

    update Popular.vue

--- src/views/Popular/Popular.vue
+++ src/views/Popular/Popular.vue
@@ -1,46 +1,79 @@
-<template>
-  <section v-if="cardsList?.length > 0" class="container pt-5">
-    <div class="row justify-content-center mb-4">
-      <div class="col-xl-6 col-lg-8 col-12">
-        <Search 
-          :type="type"
-        />
-      </div>
-    </div>
-
-    <h1 class="mb-3 text-center">Popular {{ type }}</h1>
-
-    <div class="row justify-content-center mb-3">
-      <div v-for="card in cardsList" :key="card.i" class="col-xl-2 col-lg-3 col-sm-4 col-8 py-3">
-        <Card :data="card"></Card>
-      </div>
-    </div>
-
-    <div class="row justify-content-center">
-      <button 
-        v-if="cardsList.length < 100"
-        class="btn btn-primary loadMore col-auto mb-3"
-        @click="loadMore"
-      >Load more</button>
-    </div>
-  </section>
-  <ConnectionError v-else-if="cardsList === 'Error'"></ConnectionError>
-  <Loader v-else size="180" />
-</template>
-
-
-<script>
+<script setup lang="ts">
 import axios from 'axios'
-import { ref, watch } from 'vue'
+import { ref, watch, inject } from 'vue'
 import { useStore } from 'vuex'
 import { useRoute, useRouter } from 'vue-router'
+import { Key } from '@/store'
 
 import Loader from '@/components/Loader/Loader.vue'
 import Card from '@/components/Card/Card.vue'
 import ConnectionError from '@/components/ConnectionError/ConnectionError.vue'
 import Search from '@/components/Search/Search.vue'
 
-export default {
+const key = inject<Key>('key')
+const { getters } = useStore(key)
+const route = useRoute()
+const router = useRouter()
+
+const cardsList = ref<any | null>(null)
+const loadTo = ref(21)
+
+type Content = 'movies' | 'series'
+const contentType = ref<Content>(route.params.type as Content | 'movies')
+const storageItem = contentType.value === 'movies' ? 'popularMovies' : 'popularSeries'
+const urlRequest = contentType.value === 'movies' ? 'MostPopularMovies' : 'MostPopularTVs'
+
+const fetchData = async () => {
+  try {
+    const res = await axios.get(`${getters.url}/${urlRequest}/${getters.apiKey}`)
+    const isOk = res.data.errorMessage !== '' || res.status !== 200
+    if (isOk) throw new Error('Error')
+
+    const list: any[] = []
+    res.data.items.forEach((item: any, i: number) => {
+      list.push({
+        title: item.title,
+        year: item.year,
+        imDbRating: item.imDbRating,
+        image: item.image,
+        id: item.id,
+        type: contentType.value,
+        i
+      })
+    })
+    //console.log('assign cardsList', type.value)
+    cardsList.value = list.slice(0, loadTo.value)
+    localStorage.setItem(storageItem, JSON.stringify({ list, time: Date.now() }))
+    return null
+  } catch (err) {
+    cardsList.value = 'Error'
+    return null
+  }
+}
+
+const getData = () => {
+  // if data is stored locally, retrieve it to the ref 
+  if (JSON.parse(localStorage.getItem(storageItem) as string)) {
+    const localData = JSON.parse(localStorage.getItem(storageItem) as string)
+    cardsList.value = localData.list.slice(0, loadTo.value)
+    // if the data is expired (1h), fetch it again
+    if (Date.now() - localData.time > 3_600_000) {
+      fetchData()
+    }
+  } else { // fetch data if it's not stored locally
+    fetchData()
+  }
+}
+
+watch(() => route.params, getData)
+
+const loadMore = () => {
+  loadTo.value += 20
+  getData()
+}
+
+
+/*export default {
   name: 'Popular',
 
   components: { Loader, Card, ConnectionError, Search },
@@ -111,8 +144,39 @@ export default {
 
     return { cardsList, loadMore, type }
   }
-}
+}*/
 </script>
+
+
+<template>
+  <section v-if="cardsList?.length > 0" class="container pt-5">
+    <div class="row justify-content-center mb-4">
+      <div class="col-xl-6 col-lg-8 col-12">
+        <Search 
+          :type="contentType"
+        />
+      </div>
+    </div>
+
+    <h1 class="mb-3 text-center">Popular {{ contentType }}</h1>
+
+    <div class="row justify-content-center mb-3">
+      <div v-for="card in cardsList" :key="card.i" class="col-xl-2 col-lg-3 col-sm-4 col-8 py-3">
+        <Card :data="card"></Card>
+      </div>
+    </div>
+
+    <div class="row justify-content-center">
+      <button 
+        v-if="cardsList.length < 100"
+        class="btn btn-primary loadMore col-auto mb-3"
+        @click="loadMore"
+      >Load more</button>
+    </div>
+  </section>
+  <ConnectionError v-else-if="cardsList === 'Error'"></ConnectionError>
+  <Loader v-else size="180" />
+</template>
 
 
 <style lang='scss' scoped>
